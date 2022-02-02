0eca5846a5c99112dd1274599dde9261611dbc8f
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-02-02 17:15:11 +0530

    init

--- controller.js
+++ controller.js
@@ -34,16 +34,10 @@ module.exports = async (app = require('express')()) => {
             const body = Buffer.from(fileResponse.data).toString('base64');
             const file = {
                 name: originalname,
-                contentType: 'application/octet-stream',
                 body
             };
             const encodedFileName = encodeURIComponent(file.name).replace(/['()]/g, escape).replace(/\*/g, '%2A').replace(/%(?:7C|60|5E)/g, unescape);
-            response.set({
-                'Content-Disposition': `inline; filename*=UTF-8''${encodedFileName}`,
-                'Content-Type': file.contentType,
-                'Last-Modified': (new Date()).toUTCString(),
-                'Cache-Control': 'no-cache'
-            });
+            response.set({ 'Content-Disposition': `attachment; filename*=UTF-8''${encodedFileName}` });
             return response.send(file.body);
         } catch (error) {
             return response.send(JSON.stringify(error));
