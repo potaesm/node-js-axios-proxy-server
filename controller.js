fd020bba1747ffb4d753d9e3e93c1956af9ec475
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-02-02 17:03:09 +0530

    init

---
+++ controller.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+module.exports = async (app = require('express')()) => {
+    app.get('/', async (request, response) => {
+        try {
+            const { url } = request.query;
+            const axiosResponse = await axios.default.get(url);
+            return response.send(axiosResponse.data);
+        } catch (error) {
+            return response.send(JSON.stringify(error));
+        }
+    });
+    app.post('/', async (request, response) => {
+        try {
+            const { url } = request.query;
+            const axiosResponse = await axios.default.post(url, request.body);
+            return response.send(axiosResponse.data);
+        } catch (error) {
+            return response.send(JSON.stringify(error));
+        }
+    });
+    app.get('/download-file/', async (request, response) => {
+        try {
+            const { url, filename } = request.query;
+            if (!url) {
+                return response.send('');
+            }
+            let originalname = filename;
+            if (!originalname) {
+                const chunks = url.split('/');
+                originalname = chunks[chunks.length - 1];
+            }
+            const fileResponse = await axios.default.get(decodeURIComponent(url), { responseType: 'arraybuffer' });
+            const body = Buffer.from(fileResponse.data, 'binary');
+            const file = {
+                name: originalname,
+                body
+            };
+            const encodedFileName = encodeURIComponent(file.name).replace(/['()]/g, escape).replace(/\*/g, '%2A').replace(/%(?:7C|60|5E)/g, unescape);
+            response.set({ 'Content-Disposition': `attachment; filename*=UTF-8''${encodedFileName}` });
+            return response.send(file.body);
+        } catch (error) {
+            return response.send(JSON.stringify(error));
+        }
+    });
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const Controller = require('./controller');
+
+const PORT = process.env.PORT || process.env.npm_package_config_port || 3000;
+
+function startExpress() {
+    const app = express();
+    app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+    app.use(express.json({ limit: '50mb' }));
+    app.use(cors({ origin: true }));
+    app.use(helmet());
+    Controller(app);
+    return app;
+}
+
+const cluster = require('cluster');
+if (cluster.isMaster) {
+    const numWorkers = require('os').cpus().length;
+    for (let i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', function (worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    })
+
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    })
+} else {
+    startExpress().listen(PORT);
+}
