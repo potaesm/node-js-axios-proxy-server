4dddb9ebc6e96f15d3d5f98a6e379306e49e37bb
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-02-02 19:27:21 +0530

    Add railway

--- heroku-node.sh
+++ heroku-node.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! "$WEB_MEMORY" = "" ]; then
-if [ $WEB_MEMORY -le 512 ]; then
+  if [ $WEB_MEMORY -le 512 ]; then
     NODE_FLAGS="--max_semi_space_size=2 --max_old_space_size=256"
-elif [ $WEB_MEMORY -le 768 ]; then
+  elif [ $WEB_MEMORY -le 768 ]; then
     NODE_FLAGS="--max_semi_space_size=8 --max_old_space_size=512"
-elif [ $WEB_MEMORY -le 1024 ]; then
+  elif [ $WEB_MEMORY -le 1024 ]; then
     NODE_FLAGS="--max_semi_space_size=16 --max_old_space_size=1024"
-fi
+  fi
 fi
 
 echo "Starting app with command:"
